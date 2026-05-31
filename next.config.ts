6ad5fdd6b78965c19230426f66bbc9573adcde50
Jiwon Choi <devjiwonchoi@gmail.com>
2026-06-01 00:24:42 +0200

    config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: true,
+  experimental: {
+    parallelServerFunctions: true,
+  },
 }
 
 export default nextConfig;
